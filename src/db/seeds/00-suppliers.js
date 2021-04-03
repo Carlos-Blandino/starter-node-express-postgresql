c6c728d0e53e5c34f5ef09e34d52fbaac3b96f43
Carlos Blandino <cablandino66@gmail.com>
2021-04-03 09:19:06 -0700

    adding crud with knex

---
+++ src/db/seeds/00-suppliers.js
@@ -0,0 +1,10 @@
+
+const suppliers = require("../fixtures/suppliers");
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE suppliers RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("suppliers").insert(suppliers);
+    });
+};
